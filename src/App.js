7cb2cfe0b1755dcab70cc9bdbd970576b19973c4
linux <freelancertanvirhossain@gmail.com>
2022-09-13 17:25:01 +0600

    i am added a another file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-Router-dom';
 import './App.css';
-
+import Home from './components/Home/Home.js';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <Router>
+        <Route path='/' component={Home} />
+        {/* <Route path='/about' component={About} />
+        <Route path='/shop' component={Shop} /> */}
+      </Router>
+    
+  )
 }
 
 export default App;
---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Shop = () => {
+    return (
+        <div>
+            Welcome To Shop Page
+        </div>
+    );
+};
+
+export default Shop;
